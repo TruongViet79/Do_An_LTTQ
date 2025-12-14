6c315fa59a0cbfb76866657e2d0e4ee99a72dcb3
May1804 <24521360@gm.uit.edu.vn>
2025-12-14 14:28:46 +0700

    FE

---
+++ Do_An_LTTQ/Do_An_LTTQ/View/UserPage/DashboardPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Do_An_LTTQ.View.UserPage
+{
+    /// <summary>
+    /// Interaction logic for DashboardPage.xaml
+    /// </summary>
+    public partial class DashboardPage : Page
+    {
+        public DashboardPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FriendList_MouseEnter(object sender, MouseEventArgs e)
+        {
+            FriendList.Width = 200;
+        }
+
+        private void FriendList_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (!FriendList.IsMouseOver) { FriendList.Width = 100; }
+        }
+    }
+}
--- Do_An_LTTQ/Do_An_LTTQ/obj/Debug/net8.0-windows/Do_An_LTTQ.AssemblyInfo.cs
+++ Do_An_LTTQ/Do_An_LTTQ/obj/Debug/net8.0-windows/Do_An_LTTQ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Do_An_LTTQ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aadd7d1bb4a54b02b831cbe40871abf988b65a52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d9100ca90e3781add9dc0542ba81ead11401c66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Do_An_LTTQ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Do_An_LTTQ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
